b72dedd164c32642c711054414911bb93c775019
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-03-15 21:52:18 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,16 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
-	char txt[8] = "_putchar";
-	int i;
+	_putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
-	for (i = 0; i < 8; i++)
-	{
-		putchar(txt[i]);
-	}
-	putchar('\n');
 	return (0);
 }
