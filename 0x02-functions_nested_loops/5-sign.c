6e5d78b31852f46e0ae2f0aa6e69073b85e7228a
Stella Stella <stellanic2011@gmail.com>
2022-03-16 11:17:15 +0300

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: Make reference to the ASCII
+ *
+ * Return: returns 1 and prints 0 if n is greater than 0, return 0 when it is 0 and prints 0 and returns -1 prints - if n is less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
