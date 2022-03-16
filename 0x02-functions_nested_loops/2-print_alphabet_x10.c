5dd074558540551b5910572d8f39c3dda08dc9c3
Stella Stella <stellanic2011@gmail.com>
2022-03-16 09:13:27 +0300

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char s;
+	int i = 0;
+	
+	while (i <= 9)
+	{
+		for (s = 'a'; s <= 'z'; s++)
+		{
+			_putchar(s);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
+
