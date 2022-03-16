f9d04a3b725ef3594257a6a71d6e4e7ca78ffa83
Stella Stella <stellanic2011@gmail.com>
2022-03-16 09:45:18 +0300

    lowercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks lowercase characters
+ * @c: Character is checked here ASCII code considered
+ *
+ * Return: 1 for lowercase or 0 for otherwise
+ */
+int _islower(int s)
+{
+	if (s >= 97 && s <= 122)
+	{
+		return (1);
+	{
+	return (0);
+}
