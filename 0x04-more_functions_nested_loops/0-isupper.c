c70be410b42b9a4f66a1099e34f732e79e1c8697
Stella Stella <stellanic2011@gmail.com>
2022-03-17 11:51:28 +0300

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks the uppercase
+ *
+ * Return: 0 lowercase c 1 uppercase c
+ */
+int _isupper(int c);
+{
+	if ( c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
