bd642a2398aebd112d2c21057d0bd0d8049cfb80
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-03-18 14:02:34 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,21 +5,23 @@
  * @size: square side size
  * Return: nothing
  */
-
 void print_square(int size)
 {
-	int row, col;
+	int x, y;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (col = 0; col < size; col++)
+		for (x = 0; x < size; x++)
 		{
-			for (row = 0; row < size; row++)
+			for (y = 0; y < size; y++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
+
 			_putchar('\n');
 		}
 	}
