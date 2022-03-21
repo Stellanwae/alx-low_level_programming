94fc00661caeeb97f95b15014036d93d0c7c31d3
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-03-21 23:54:03 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,16 +8,11 @@
  */
 void _puts(char *str)
 {
-	int counter = 0;
+	char s1[] = "Hello";
+	char s2[] = "World!";
 
-	while (counter >= 0)
-	{
-		if (str[counter] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[counter]);
-		counter++;
-	}
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	return (0);
 }
