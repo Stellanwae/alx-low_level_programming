35496453cd95e7750b6448f7f87633935298f15e
Stella <stellanic2011@gmail.com>
2022-03-21 22:45:19 +0300

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * swap_int - swaps integers
+ *
+ * @a: An int to swap
+ * @b: A second int to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+
