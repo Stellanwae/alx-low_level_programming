12ac418b1f135fdef688b393585dd71c451da04e
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-05-01 17:01:00 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+*
+*
+*
+*/
+
+int main()
+{
+  int n, p, s;
+  s = 0;
+  for(n = 0; n < 1024; n++)
+  {
+    if(n % 3 == 0 || n % 5 == 0)
+    {
+      s += n;
+      if(n == 1023)
+      {
+        printf("%d\n", s);
+      }
+    }
+  }
+}
