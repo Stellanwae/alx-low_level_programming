bf6b4608c7845eb939591b28da880b2c340bcf41
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-05-01 17:25:48 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,16 +11,16 @@ int main(void)
 {
  int n, s;
  s = '0';
-  for (n = 0; n < 1024; n++)
-  {
-    if (n % 3 == 0 || n % 5 == 0)
-    {
-      s += n;
-      if (n == 1023)
-      {
-        printf("%d\n",s);
-      }
-    }
-  }
+ for (n = 0; n < 1024; n++)
+ {
+   if (n % 3 == 0 || n % 5 == 0)
+   {
+     s += n;
+     if (n == 1023)
+     {
+       printf("%d\n",s);
+     }
+   }
+ }
   return 0;
 }
