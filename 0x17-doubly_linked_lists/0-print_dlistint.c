17f5d7109343a041130b397a50b23e9c2e4b56d6
Stellanwae <stellanic2011@gmail.com>
2022-06-10 04:15:02 +0300

    moved from high level

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "list.h"
+/**
+ * print_dlistint - Prints the list of the d linked list
+ * @h: The doubly linked list
+ *
+ * Return: Number of nodes
+ */
+
+ size_t print_dlistint(const dlistint_t *h);
+{
+  int length;
+  
+  for (length = 0; h != NULL; lenghth++)
+  {
+    printf("%d\n", h->n);
+    h = h->next;
+  }
+  return(length);
+}
