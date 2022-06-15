24cac4108c453db6f7772bd771a74884ab2b8867
Stellanwae <stellanic2011@gmail.com>
2022-06-15 16:08:58 +0300

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - gives sqrt of a number
+ * @n: natural number to calc sqrt
+ *
+ * Return: Void
+ */
+int _sqrt_recursion(int n)
+{
+	return(_sqcheck(n, 1));
+}
+
+/**
+ * _sqcheck - gets natural sqr rt
+ * @n: the natural number
+ * @k: iterated number
+ *
+ * Return: sqrt of natural number
+ */
+int _sqcheck(int n, int k)
+{
+	int w = k * k;
+
+	if (w > n)
+	{
+		return (-1);
+	}
+	if (w == n)
+	{
+		return (k);
+	}
+	return (_sqcheck(n, k + 1));
+}
