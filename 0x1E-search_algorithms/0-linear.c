e8445566c95d9dac4b75914a4ae07a9fb5ad4445
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-04-13 18:03:51 +0300

    Create 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include stdio.h
+#include search_algos.h
+
+/**
+* a linear search - looks for value in an array
+* @array: pointer for the 1st element of array
+* @value: the value
+*
+* Return: 
+*/
+int linear_search(int *array, size_t size, int value)
+{
+  size_t j;
+  
+	if (array == NULL)
+		return (-1);
+
+	for (j = 0; j < size; j++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+
+	return (-1);
+}
